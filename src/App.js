1fed37f00bdcb63c0ebe8a33509c5c57fdd9b897
polarzero <0xpolarzero@gmail.com>
2023-01-22 18:59:35 +0100

    Remove drawer setup

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import { Canvas } from '@react-three/fiber';
 import { ConfigProvider, theme } from 'antd';
 import Graph from 'src/components/Graph';
 import Interface from 'src/components/Interface';
-import { useData, useEnv, useInterface } from 'src/stores';
+import { useData, useEnv } from 'src/stores';
 
 const App = () => {
   const { setData, loaded, error } = useData();
   const { initialCameraPosition } = useEnv();
 
-  const { setDrawer } = useInterface();
-
   useEffect(() => {
     setData();
-
-    setDrawer(null, null, 'global');
   }, []);
 
   return (
